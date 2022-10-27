fcbbce3da0fce8f3c82eeb9423bc73ef0f3bd9ad
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-10-27 16:38:08 +0700

    Initial commit

---
+++ lib/TableData/Business/ID/BPOM/FoodType.pm
@@ -0,0 +1,56 @@
+package ## no critic: Modules::RequireFilenameMatchesPackage
+    # hide from PAUSE
+    TableDataRole::Business::ID::BPOM::FoodType;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny;
+with 'TableDataRole::Source::AOA';
+
+around new => sub {
+    require App::BPOMUtils::Table;
+
+    my $orig = shift;
+    my $self = shift;
+
+    $orig->(
+        $self,
+        column_names => [ sort {
+            $App::BPOMUtils::Table::meta_idn_bpom_jenis_pangan->{fields}{$a}{pos} <=>
+            $App::BPOMUtils::Table::meta_idn_bpom_jenis_pangan->{fields}{$b}{pos};
+        } keys %{ $App::BPOMUtils::Table::meta_idn_bpom_jenis_pangan->{fields} } ],
+        aoa => $App::BPOMUtils::Table::data_idn_bpom_jenis_pangan,
+        @_,
+    );
+};
+
+package TableData::Business::ID::BPOM::FoodType;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny::With;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+with 'TableDataRole::Business::ID::BPOM::FoodType';
+
+# STATS
+
+1;
+# ABSTRACT: Food types in BPOM processed food division
+
+=head1 DESCRIPTION
+
+Keyword: BPOM, pangan olahan, jenis pangan
+
+
+=head1 SEE ALSO
+
+L<TableData::Business::ID::BPOM::FoodCategory>
---
+++ lib/TableDataBundle/Business/ID/BPOM.pm
@@ -0,0 +1,26 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package TableDataBundle::Business::ID::BPOM;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of TableData:: modules related to Indonesia's BPOM (Badan Pengawas Obat dan Makanan, or the National Agency or Drug and Food Control)
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<TableData>:: modules:
+
+# INSERT_MODULES_LIST /^TableData::/
+
+
+=head1 SEE ALSO
+
+L<TableData>
+
+L<https://pom.go.id>
+
+=cut
